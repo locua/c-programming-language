dcc5c3c86b831dbad4ee3d2add3fc831ed265704
Louis <poes997@gmail.com>
2021-01-25 17:36:39 +0000

    for loop

--- c1/hello.c
+++ c1/hello.c
@@ -4,15 +4,16 @@ int main()
 {
     printf("Hello!\n");
 
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;
     upper = 300;
     step  = 20;
 
-    fahr = lower;
-    while(fahr<=upper)
+    //fahr = lower;
+    //while(fahr<=upper)
+    for(fahr = 0; fahr <=300; fahr+=20)
     {
         celsius = (5.0/9.0) *(fahr-32);
         printf("%3.0f \t%6.1f\n", fahr, celsius);
