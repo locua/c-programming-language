b44e7fb20150e411c3980cb48036c2736d72ad58
Louis <poes997@gmail.com>
2021-01-25 17:26:47 +0000

    floating point conversion

--- c1/hello.c
+++ c1/hello.c
@@ -14,8 +14,8 @@ int main()
     fahr = lower;
     while(fahr<=upper)
     {
-        celsius = 5*(fahr-32)/9;
-        printf("%3d\t%6d\n", fahr, celsius);
+        celsius = (5.0/9.0) *(fahr-32);
+        printf("%3.0f \t%6.1f\n", fahr, celsius);
         fahr+=step;
     }
 }
